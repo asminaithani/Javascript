03c8168cf2452c9d9d22b1a05257a781831dd63b
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2024-07-14 12:19:18 +0000

    conversions modified

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "44"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
  
 //"33" => 33
 // "33abc" => NaN
@@ -14,5 +14,36 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+//console.log(booleanisLoggedIn);
+
+let someNum = 34
+let stringNumber = String(someNum)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue);
+
+//console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Asmi"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
 
