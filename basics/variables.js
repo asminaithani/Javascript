57461cededd33703da7f139a1793758fccdd4eac
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2024-07-14 11:14:50 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId  = 12277
+let accountEmail = "bgvjr@gmail.com"
+var accountPass = "@gdjw"
+accountCity = "Jaipur"
+
+
+// accountId = 74 // not allowed\
+
+/*prefered not to use var 
+issue in block scope and functional scope*/
+
+accountEmail = "bcyerh.com"
+accountPass = "7372"
+accountCity = "roorkee"
+
+console.table([accountEmail , accountId, accountPass ,accountCity]);
