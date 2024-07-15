688227ac922a0cac5b99f1dbae79ade195663f5e
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2024-07-15 17:40:12 +0000

    datatype summary

---
+++ basics/04_datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 types 
+//String
+//Number
+//Boolean
+//null, undefined,symbol, bigint
+
+// Reference - non primitive
+// array, object, functions
+
+const arr = ["asmi", "shubh", "ekanshu"];
+
+let myObj = {
+    name: "asmi",
+    course: "btech",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("hello")
+}
+
+console.log(typeof myFunction);
+
+
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
